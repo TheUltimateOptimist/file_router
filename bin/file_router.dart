1ac10113b0aeef612eb1db9bb162fb20c0d912c1
TheUltimateOptimist <digitalConfidence@web.de>
2023-05-16 17:38:14 +0200

    integrated source generation into file_router.dart

--- bin/file_router.dart
+++ bin/file_router.dart
@@ -1,8 +1,10 @@
 import 'extractor.dart';
+import 'generator.dart';
 
 void main(List<String> args) {
   final routes = extractRoutes();
-  for (final route in routes) {
-    print(route.toCustomString(""));
-  }
+  // for (final route in routes) {
+  //   print(route.toCustomString(""));
+  // }
+  generateSource(routes);
 }
